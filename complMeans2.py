58085506207ffda10c1e71c6647c2ad24eeec426
Lyes HAMIDOUCHE <lyes@magency.fr>
2017-06-19 20:05:21 +0200

    ComplMeans

--- complMeans2.py
+++ complMeans2.py
@@ -95,8 +95,10 @@ for ns in res["NetSize"].unique():
                         #print df6
                         if al == "simu":
                             df6.rename(columns={"Completion time(s)": 'Batt' } , inplace = True )
-                        else:
+                        elif al == "simu3":
                             df6.rename(columns={'Completion time(s)':'Batt + Bw'} , inplace = True )
+                        else:
+                            df6.rename(columns={'Completion time(s)':'Batt + Bw 2'} , inplace = True )
 
                         columns.append(df6)
                     out = pd.concat(columns,axis=1)
@@ -120,11 +122,59 @@ for ns in res["NetSize"].unique():
                         #print df6
                         if al == "simu":
                             df6.rename(columns={"Completion time(s)": 'Batt' } , inplace = True )
-                        else:
+                        elif al == "simu3":
                             df6.rename(columns={'Completion time(s)':'Batt + Bw'} , inplace = True )
+                        else:
+                            df6.rename(columns={'Completion time(s)':'Batt + Bw 2'} , inplace = True )
 
                         columns.append(df6)
                     out = pd.concat(columns,axis=1)
                     print out 
                     out.to_csv(folder+os.sep+"out"+os.sep+'ComplBadFixed-'+str(ns)+'-'+str(cs)+'-'+str(mp)+'-'+str(fc)+'-'+str(int(p))+'-byGoodDevices.csv',sep=",")
                     Plot.plot_lines(out,folder+os.sep+"out"+os.sep+'ComplBadFixed-'+str(ns)+'-'+str(cs)+'-'+str(mp)+'-'+str(fc)+'-'+str(int(p))+'-byGoodDevices.eps',{'xaxis_label':'Fast devices (percent)'})
+
+
+
+
+
+for ns in res["NetSize"].unique():
+    df = res[res["NetSize"]==ns]
+    del df["NetSize"]
+    for cs in df["Chunk count"].unique():
+        df2 = df[df['Chunk count']==cs]
+        del df2['Chunk count']
+        for fc in df2['Population'].unique():
+            df3 = df2[df2['Population'] == fc]
+            del df3['Population']
+            #print df3
+            for mp in df3['MaxPar'].unique():
+                df4 = df3[df3['MaxPar'] == mp]
+                del df4['MaxPar']
+                #print df4
+                # Fix good devices
+                for p in df4['BadDevices'].unique():
+                    df5 = df4[df4['BadDevices'] == p]
+                    del df5['BadDevices']
+                    #print df5
+                    indexName='FastC'
+                    columns =[]
+                    for al in df5['Algorithm'].unique():
+                        df6 = df5[df5['Algorithm']==al]
+                        del df6["Algorithm"]
+                        df6.index = df6[indexName]
+                        del df6[indexName]
+                        #print df6
+                        if al == "simu":
+                            df6.rename(columns={"Completion time(s)": 'Batt' } , inplace = True )
+                        elif al == "simu5":
+                            df6.rename(columns={'Completion time(s)':'Batt + Bw'} , inplace = True )
+                            columns.append(df6)
+                        else:
+                            df6.rename(columns={'Completion time(s)':'Batt + Bw 2'} , inplace = True )
+
+                       
+                    out = pd.concat(columns,axis=1)
+                    print out
+                    print folder+os.sep+"out"+os.sep+'ComplGoodBadFixed-'+str(ns)+'-'+str(cs)+'-'+str(mp)+'-'+str(fc)+'-'+str(int(p))+'-byFastC.csv'
+                    out.to_csv(folder+os.sep+"out"+os.sep+'ComplGoodBadFixed-'+str(ns)+'-'+str(cs)+'-'+str(mp)+'-'+str(fc)+'-'+str(int(p))+'-byFastC.csv',sep=",")
+                    Plot.plot_lines(out,folder+os.sep+"out"+os.sep+'ComplGoodBadFixed-'+str(ns)+'-'+str(cs)+'-'+str(mp)+'-'+str(fc)+'-'+str(int(p))+'-byFastC.eps',{'xaxis_label':'FastC (percent)'})
